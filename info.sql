80db86c90934f33a3b5cc11f37b5cd8550c0c2ce
cattuong28597 <cattuong28597@gmail.com>
2021-11-11 14:59:27 +0700

    lan1

---
+++ info.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE myLibrary;
+
+USE myLibrary;
+
+CREATE TABLE Books(
+IdBook VARCHAR(50) PRIMARY KEY,
+BookName VARCHAR(220),
+Kind VARCHAR(50),
+Author VARCHAR(50),
+Quantity INT
+);
+
+CREATE TABLE Borrowers(
+IdBorrower INT AUTO_INCREMENT PRIMARY KEY,
+BorrowerName VARCHAR(220),
+Address VARCHAR(220),
+PhoneNumber VARCHAR(15),
+Email VARCHAR(40),
+IdBook VARCHAR(50),
+FOREIGN KEY (IdBook) REFERENCES Books(IdBook)
+);
+
+ 
